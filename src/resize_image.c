ef63671fe684fed6b3336c81826b7ad58b76a528
Goodness Duru <goodyduru@gmail.com>
2021-02-06 22:55:44 +0100

    Do bilinear interpolation.

--- src/resize_image.c
+++ src/resize_image.c
@@ -28,13 +28,59 @@ image nn_resize(image im, int w, int h)
 
 float bilinear_interpolate(image im, float x, float y, int c)
 {
-    // TODO
-    return 0;
+    /*         top_middle
+        *--------------------* pixel
+        |  left    | right   |
+        |          |         | top
+        |          |         |
+        |----------*---------- 
+        |          |         |
+        |          |         | bottom
+        *--------------------*
+                bottom_middle
+    */
+    float left, right, top, bottom, top_middle, bottom_middle, top_right_pixel,
+          top_left_pixel, bottom_right_pixel, bottom_left_pixel, pixel;
+    int floor_x, floor_y, ceil_x, ceil_y;
+    floor_x = (int) floorf(x);
+    ceil_x = (int) ceilf(x);
+    floor_y = (int) floorf(y);
+    ceil_y = (int) ceilf(y);
+    left = x - floor_x;
+    right = ceil_x - x;
+    top = y - floor_y;
+    bottom = ceil_y - y;
+    // Get the weighted average of the top right pixel and the top left pixel
+    // Let the weight be the swapped distance of the point from the pixels.
+    top_left_pixel = get_pixel(im, floor_x, floor_y, c);
+    top_right_pixel = get_pixel(im, ceil_x, floor_y, c);
+    top_middle = left * top_right_pixel + right * top_left_pixel;
+
+    bottom_left_pixel = get_pixel(im, floor_x, ceil_y, c);
+    bottom_right_pixel = get_pixel(im, ceil_x, ceil_y, c);
+    bottom_middle = left * bottom_right_pixel + right * bottom_left_pixel;
+
+    pixel = bottom * top_middle + top * bottom_middle;
+    return pixel;
 }
 
 image bilinear_resize(image im, int w, int h)
 {
-    // TODO
-    return make_image(1,1,1);
+    int i, j, k;
+    float a_x, a_y, b_x, b_y, pixel;
+    image new_img = make_image(w, h, im.c);
+    a_x = (float)im.w / w;
+    b_x = -0.5 + 0.5 * a_x;
+    a_y = (float)im.h / h;
+    b_y = -0.5 + 0.5 * a_y;
+    for ( i = 0; i < w; i++ ) {
+        for ( j = 0; j < h; j++ ) {
+            for ( k = 0; k < im.c; k++ ) {
+                pixel = bilinear_interpolate(im, a_x * i + b_x, a_y * j + b_y, k);
+                set_pixel(new_img, i, j, k, pixel);
+            }
+        }
+    }
+    return new_img;
 }
 
